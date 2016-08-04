3f7f14fd4f42a8809b1f81217c5029248a31d9d3
Kaitlyn Adelaide & Spencer Dezart-Smith <pair+kaitlynmcpartland.spencerdezartsmith@devbootcamp.com>
2016-08-04 14:25:57 -0700

    Fix password_digest

--- movie_database/db/migrate/20160803223559_create_movies.rb
+++ movie_database/db/migrate/20160803223559_create_movies.rb
@@ -3,7 +3,9 @@ class CreateMovies < ActiveRecord::Migration
     create_table :movies do |t|
       t.string :title
       t.text :summary
+      t.string :poster_path
       t.integer :category_id
+      t.date :release_date
 
       t.timestamps null: false
     end
