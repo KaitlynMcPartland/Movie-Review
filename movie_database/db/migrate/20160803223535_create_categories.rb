ab21905b501357e344524890ef81d81553c55a0b
Kaitlyn Adelaide & Spencer Dezart-Smith <pair+kaitlynmcpartland.spencerdezartsmith@devbootcamp.com>
2016-08-03 19:29:07 -0700

    Add column to category model

--- movie_database/db/migrate/20160803223535_create_categories.rb
+++ movie_database/db/migrate/20160803223535_create_categories.rb
@@ -2,6 +2,7 @@ class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
       t.string :name,  presence: true
+      t.integer :movie_db_id, presence: true
 
       t.timestamps null: false
     end
