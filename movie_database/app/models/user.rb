bedebaab4433324269f831a3147445a90a4a7828
Melanie Hahn <melaniesarah@gmail.com>
2016-08-03 19:48:32 -0700

    Add devise user authentication

--- movie_database/app/models/user.rb
+++ movie_database/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :reviews, foreign_key: 'reviewer_id'
   has_many :review_votes, foreign_key: 'voter_id'
   has_many :comments, foreign_key: 'commenter_id'
