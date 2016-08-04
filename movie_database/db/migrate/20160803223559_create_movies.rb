a7a5b9acd6f3566d4c84d948443fc7991ca584da
Kaitlyn Adelaide & Spencer Dezart-Smith <pair+kaitlynmcpartland.spencerdezartsmith@devbootcamp.com>
2016-08-03 17:43:44 -0700

    Add validations to migrations

--- movie_database/db/migrate/20160803223559_create_movies.rb
+++ movie_database/db/migrate/20160803223559_create_movies.rb
@@ -1,9 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-      t.string :title
-      t.text :summary
-      t.integer :category_id
+      t.string :title, presence: true
+      t.text :summary, presence: true
+      t.string :poster_path, presence: true
+      t.integer :category_id, presence: true
+      t.date :release_date, presence: true
 
       t.timestamps null: false
     end
